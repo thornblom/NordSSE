3340691ff1c8dd5a9ce88d09698a275c94bef624
majilik <majilik@live.se>
2016-04-26 09:30:32 +0200

    Client stuff

---
+++ HelloWorldSSE/Scripts/ssetest.js
@@ -0,0 +1,16 @@
+﻿function main() {
+    var eventURL = location.protocol + "//" + location.host + "/SSE";
+    console.log(eventURL);
+
+    var evtSrc = new EventSource(eventURL);
+
+    evtSrc.onmessage = function (msg) {
+        listWrite(msg.data);
+    }
+}
+
+function listWrite(data) {
+    $("#messageHolder").append("<li>" + data + "</li>");
+}
+
+$(document).ready(main);
